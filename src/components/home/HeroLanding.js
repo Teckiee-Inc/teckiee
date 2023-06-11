5b4c718692b60e80273e138a127ed1d18d6bd143
TrevMike <preince560m@gmail.com>
2023-06-11 02:05:08 -0500

    website revamping v3

--- src/components/home/HeroLanding.js
+++ src/components/home/HeroLanding.js
@@ -1,71 +1,85 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Image from "next/image";
 import { motion, useCycle } from "framer-motion";
 import { useSelector } from "react-redux";
 
 function HeroLanding() {
-  const hlVids = [
-    { vid: "/heroVideov3.mp4", text: "Web Development" },
-    { vid: "/heroVideov2.mp4", text: "Marketing" },
-    { vid: "/heroVideo.mp4", text: "Networking" },
+  const hlGifs = [
+    { vid: "/hpWebDev.gif", text: "Web Development" },
+    { vid: "/hpMarket.gif", text: "Marketing" },
+    { vid: "/hpNetwork.gif", text: "Networking" },
   ];
-  const solutionsInfo = useSelector(state => state.hp.solutionsInfo);
-  const [currentVideo, cycleVideos] = useCycle(...hlVids);
-  const [textHeader, setTextHeader] = useState(currentVideo.text);
+
+  const solutionsInfo = useSelector((state) => state.hp.solutionsInfo);
+  const [currentGif, cycleGifs] = useCycle(...hlGifs);
+  const [textHeader, setTextHeader] = useState(currentGif.text);
+  const isLoadedRef = useRef(false);
+  const cycleGifsRef = useRef(cycleGifs);
+
+  const loadHandler = () => {
+    isLoadedRef.current = true;
+  };
 
   useEffect(() => {
-    setTextHeader(currentVideo.text);
+    setTextHeader(currentGif.text);
 
     const letters = "abcdefghijklmnopqrstuvwxyz";
     let iterations = 0;
 
     const interval = setInterval(() => {
-      const randomText = currentVideo.text
+      const randomText = currentGif.text
         .split("")
         .map((item, index) => {
           if (index < iterations) {
-            return currentVideo.text[index];
+            return currentGif.text[index];
           }
           return letters[Math.floor(Math.random() * 26)];
         })
         .join("");
 
-      if (iterations >= currentVideo.text.length) {
+      if (iterations >= currentGif.text.length) {
         clearInterval(interval);
       }
       iterations += 1 / 3;
       setTextHeader(randomText);
     }, 30);
 
-    return () => clearInterval(interval);
-  }, [currentVideo.text]);
+    const gifLooper = setInterval(() => {
+      if (!isLoadedRef.current) {
+        cycleGifsRef.current();
+      }
+      isLoadedRef.current = false;
+    }, 3000);
+
+    const cleanUp = () => {
+      clearInterval(interval);
+      clearInterval(gifLooper);
+    };
+
+    return cleanUp;
+  }, [currentGif]);
 
   return (
     <header className="pt-16">
       <div className="relative">
         <div className="h-48">
           <motion.div
-            className="w-full min-h-full"
+            key={currentGif.vid}
+            className="w-full min-h-full relative"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            transition={{ duration: 2, ease: "easeIn", type: "tween" }}
+            transition={{ duration: 2, ease: "easeInOut" }}
           >
-            <motion.video
-              autoPlay
-              muted
-              key={currentVideo.vid}
-              src={currentVideo.vid}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1, pathLength: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{
-                duration: 2,
-                // delay: 0.5,
-                ease: "easeOut",
-                type: "tween",
+            <Image
+              className="h-auto w-auto"
+              alt="testing"
+              fill
+              src={currentGif.vid}
+              priority={true}
+              onLoadingComplete={(img) => {
+                loadHandler();
               }}
-              onEnded={cycleVideos}
             />
           </motion.div>
         </div>
